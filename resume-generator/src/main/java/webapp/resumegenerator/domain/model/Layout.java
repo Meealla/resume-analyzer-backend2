e88eaeb91539734f3cb9bbd4893453da59b21cec
Хузин Ильдар <ildar.khuzin75@mail.ru>
2024-12-27 16:28:39 +0300

    решение конфликта

--- resume-generator/src/main/java/webapp/resumegenerator/domain/model/Layout.java
+++ resume-generator/src/main/java/webapp/resumegenerator/domain/model/Layout.java
@@ -2,6 +2,7 @@ package webapp.resumegenerator.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.ElementCollection;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
